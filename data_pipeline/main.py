3a86ac3634e9007f7c4770bf0eaa695d0318952c
Khôi Nguyễn <khoinm0603@gmail.com>
2025-02-05 13:17:53 +0700

    update main

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -1,59 +1,135 @@
 from time import perf_counter
-import os
+import pandas as pd
 from dotenv import load_dotenv
+from pathlib import Path
+
 
 from CSV import create_file_data_crawling_if_not_exist
-from GetBillDetail import  GetBillDetailProcess
-from Authentication import login
+from GetBillDetail import GetBillDetailProcess
+from util import login ,TRANSFORMED_DATA,RAW_DATA,ERROR_HANDLING_FILE_PATH
 from Search import GetTotalBill
 from GetBilllID import GetBillIDProcess
 from TransformPipeline import transform_data
 from Postgre import load_bill_detail_to_db
 
 load_dotenv(dotenv_path='.env')
-def manual_pipeline(start_date,end_date,hs,trans_type):
-    login(os.getenv('USER_NAME'),os.getenv('PASSWORD'))
+
+def manual_pipeline(start_date, end_date, hscode, description,
+                    supplier, buyer, seller_country, seller_port,
+                    buyer_port, trans, qty_min, qty_max,
+                    amount_min, amount_max, uusd_min, uusd_max,
+                    transaction_type="import"):
 
     total_bills = GetTotalBill(
-        start_date= start_date,
-        end_date= end_date,
-        hscode= hs,
-        transaction_type= trans_type
+        start_date=start_date,
+        end_date=end_date,
+        hscode=hscode,
+        transaction_type=transaction_type,
+        description=description,
+        supplier=supplier,
+        buyer=buyer,
+        seller_port=seller_port,
+        seller_country=seller_country,
+        buyer_port=buyer_port,
+        trans=trans,
+        qty_min=qty_min,
+        qty_max=qty_max,
+        amount_min=amount_min,
+        amount_max=amount_max,
+        uusd_min=uusd_min,
+        uusd_max=uusd_max,
     ).execute()
 
-    if total_bills !=0:
+    if total_bills != 0:
         get_bill_id_process = GetBillIDProcess(
             start_date=start_date,
-            end_date= end_date,
-            hscode=hs,
-            type_transaction=trans_type,
-            total_bill=total_bills)
+            end_date=end_date,
+            hscode=hscode,
+            description=description,
+            buyer=buyer,
+            seller_port=seller_port,
+            seller_country=seller_country,
+            buyer_port=buyer_port,
+            trans=trans,
+            qty_min=qty_min,
+            qty_max=qty_max,
+            amount_min=amount_min,
+            amount_max=amount_max,
+            uusd_min=uusd_min,
+            uusd_max=uusd_max,
+            transaction_type=transaction_type,
+            supplier=supplier,
+            total_bill=total_bills
+        )
         get_bill_id_process.execute()
 
         # tạo file csv để caching data trong quá trình GetBillDetailimport csv
-        file_path = f'CSV/Data/{start_date}_to_{end_date}_{hs}_{trans_type}'
-        create_file_data_crawling_if_not_exist(f"{file_path}.csv")
-        get_detail_process = GetBillDetailProcess(get_bill_id_process.bill_ids,
-                                                  get_bill_id_process.trade_dates,
-                                                  get_bill_id_process.bill_id_headers,
-                                                  f'{file_path}.csv')
+        create_file_data_crawling_if_not_exist(RAW_DATA)
+        get_detail_process = GetBillDetailProcess(bill_ids=get_bill_id_process.bill_ids,
+                                                  trade_dates=get_bill_id_process.trade_dates,
+                                                  bill_id_headers=get_bill_id_process.bill_id_headers)
         get_detail_process.execute()
-        transform_data(file_path)
-        r = load_bill_detail_to_db(f'{file_path}_transformed_data.csv')
-        if r['state'] != 200:
-            print(r['message'])
-            return
-        print("Lấy dữ liệu thành công")
+
+
+def rerun_pipeline():
+    print("Rerun pipeline")
+    data = pd.read_csv(ERROR_HANDLING_FILE_PATH)
+    GetBillDetailProcess(bill_ids=data['bill_ids'].tolist(),
+                                                   trade_dates=data['trade_dates'].tolist(),
+                                                   bill_id_headers=data['bill_id_headers'].tolist()).execute()
+
 
 
 if __name__ == '__main__':
     # khai báo biến
     start = perf_counter()
-    username = os.getenv('USER_NAME')
-    password = os.getenv('PASSWORD')
+
     start_date = "2024-10-28"
     end_date = "2024-12-10"
     hs = "50"
-    trans_type = "import"
+    description = None
+    supplier = None
+    buyer = None
+    seller_port = None
+    seller_country = None
+    buyer_port = None
+    trans = None
+    qty_min = None
+    qty_max = None
+    amount_min = None
+    amount_max = None
+    uusd_min = None
+    uusd_max = None
+    transaction_type = None
+    login()
     # start process
-    manual_pipeline(start_date,end_date,hs,trans_type)
+    error_handling_file_path = Path(ERROR_HANDLING_FILE_PATH)
+    if not error_handling_file_path.exists():
+        manual_pipeline(
+            start_date=start_date,
+            end_date=end_date,
+            hscode=hs,
+            description=description,
+            supplier=supplier,
+            buyer=buyer,
+            seller_port=seller_port,
+            buyer_port=buyer_port,
+            trans=trans,
+            qty_min=qty_min,
+            qty_max=qty_max,
+            amount_min=amount_min,
+            amount_max=amount_max,
+            uusd_min=uusd_min,
+            uusd_max=uusd_max,
+            seller_country=seller_country,
+            transaction_type=transaction_type
+        )
+    else:
+        rerun_pipeline()
+
+    transform_data()
+    r = load_bill_detail_to_db(TRANSFORMED_DATA)
+    if r['state'] != 200:
+        print(r['message'])
+    else:
+        print("Lấy dữ liệu thành công")
